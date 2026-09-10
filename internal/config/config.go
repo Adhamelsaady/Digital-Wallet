7cd3d89dfe76ed445ef48efbcfeb18c36c2a3089
adhamelsaady <adhamelsaady81@gmail.com>
2026-09-10 03:21:26 +0300

    feat: implement application configuration loader for database and server settings

---
+++ internal/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"fmt" 
+	"os"
+)
+
+type Config struct {
+	DatabaseURL string
+	ServerPort  string
+}
+
+func Load () (*Config , error) {
+	DatabaseURL := os.Getenv("DATABASE_URL") 
+	if DatabaseURL == "" {
+		return nil , fmt.Errorf("DATABASE_URL enviroment variable is required")
+	}
+	ServerPort := os.Getenv("SERVER_PORT")
+	if ServerPort == "" {
+		ServerPort = "8080"
+	}
+	return &Config{
+		DatabaseURL: DatabaseURL,
+		ServerPort:  ServerPort,
+	 }, nil
+}
