336eccbbc8097800df2300697889e9c3f28e0cd0
adhamelsaady <adhamelsaady81@gmail.com>
2026-09-12 02:17:24 +0300

    feat : added the account domain

---
+++ internal/ledger/account.go
@@ -0,0 +1,55 @@
+package ledger
+
+import (
+	"time"
+	"strings"
+	"errors"
+	"github.com/google/uuid"
+)
+
+var supportedCurrencies = map[string]bool {
+	"USD": true,
+	"EGP": true,
+	"SAR": true,
+	"AED": true,
+	"EUR": true,
+}
+
+type Account struct {
+	ID uuid.UUID `json:"id"`
+	OwnerID uuid.UUID `json:"owner_id"`
+	Currency string `json:"currency`
+	Type string `json:"type"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+
+var	ErrorInvalidCurrency = errors.New("currency not supported")
+var	ErrorAccountNotFound = errors.New("account not found")
+var	ErrorInsufficientFunds = errors.New("insufficient funds")
+var	ErrorInvalidOwner = errors.New("invalid owner")
+var	ErrorInvalidType = errors.New("invalid account type")
+
+func NewAccount (ownerId uuid.UUID, currency string , accountType string) (*Account, error) {
+	if ownerId == uuid.Nil {
+		return nil , ErrorInvalidOwner
+	}
+	currency = strings.ToUpper(strings.TrimSpace(currency))
+	if !supportedCurrencies[currency] {
+		return nil , ErrorInvalidCurrency
+	}
+	accountType = strings.ToUpper(strings.TrimSpace(accountType))
+	if accountType == "" {
+		accountType = "AVAILABLE"
+	}
+	curTime := time.Now().UTC()
+	return &Account{
+		ID: uuid.New(),
+		OwnerID: ownerId,
+		Currency: currency,
+		Type: accountType,
+		CreatedAt: curTime,
+		UpdatedAt: curTime,
+	}, nil
+}
