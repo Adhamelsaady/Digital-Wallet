33c7045708632392b794048ed023c62179741f37
adhamelsaady <adhamelsaady81@gmail.com>
2026-09-14 23:09:19 +0300

    feat: add TransferParams and TransferResponse structs to ledger

---
+++ internal/ledger/transfer.go
@@ -0,0 +1,23 @@
+package ledger
+
+import "github.com/google/uuid"
+import "time"
+
+type TransferParams struct {
+	FromAccountId uuid.UUID 
+	ToAccountId uuid.UUID 
+	Amount int64 
+	Currency string
+	Description string
+}
+
+type TransferResponse struct {
+	TransactionId uuid.UUID `json:"transaction_id"`
+	FromAccountId uuid.UUID `json:"from_account_id"`
+	ToAccountId uuid.UUID `json:"to_account_id"`
+	Amount int64 `json:"amount"`
+	Currency string `json:"currency"`
+	Description string `json:"description"`
+	Status string `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+}
